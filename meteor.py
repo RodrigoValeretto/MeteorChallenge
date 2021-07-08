978b7617b170b0d85e74c942e35bad3b54992a1b
rodrigovaleretto <rodrigovaleretto@usp.br>
2021-07-08 12:39:13 -0300

    Versão final utilizando tuplas

--- meteor.py
+++ meteor.py
@@ -2,9 +2,23 @@ from PIL import Image
 from time import time
 import numpy as np
 
+
+def totuple(a):
+    # Função totuple - serve para transformar um array em uma tupla
+    # Funciona de forma recursiva então é possível transformar vários arrays
+    # aninhados, como é o caso.
+    # Créditos: Bi Rico - https://qastack.com.br/programming/10016352/convert-numpy-array-to-tuple
+    try:
+        return tuple(totuple(i) for i in a)
+    except TypeError:
+        return a
+
+
 # Abrindo imagem e gerando array de rgb dos bits
 img = Image.open('meteor_challenge_01.png')
 data = np.asarray(img)
+
+tupleData = totuple(data)
 
 # Definição das cores de comparação do programa
 # Usamos tuplas para evitar problemas de comparação com vetores
@@ -13,8 +27,7 @@ black = (0, 0, 0, 255)
 red = (255, 0, 0, 255)
 blue = (0, 0, 255, 255)
 
-# Vetor usado para gravar coordenadas dos possíveis meteoros
-coords = []
+# Vetor usado para gravar as colunas que possuem bits de água
 waterCols = []
 
 # Variáveis que salvam o indice do nível do chão e da água
@@ -35,7 +48,7 @@ h, w, _ = data.shape
 start = time()
 wcentro = int(w/2)
 for i in range(0, h):
-    if(tuple(data[i, wcentro]) == black):
+    if(tupleData[i][wcentro] == black):
         groundLvl = i
         break
 
@@ -44,25 +57,27 @@ for i in range(0, h):
     if(waterLevel != -1):
         break
     for j in range(0, w):
-        if(tuple(data[i, j]) == blue):
+        if(tupleData[i][j] == blue):
             waterLevel = i
             break
 
 # Encontra colunas em que há bits com água
 for i in range(0, w):
-    if(tuple(data[waterLevel, i]) == blue):
+    if(tupleData[waterLevel][i] == blue):
         waterCols.append(i)
 
 
 # Itera para contar quantidade de estrelas e meteoros
 # Usa o nível do chão para parar de iterar pois a partir do chão não há mais estrelas
+# Quando encontra um meteoro verifica se o indice de sua coluna está contido no array
+# com os indices de coluna que contém água, se sim significa que o meteoro vai cair na água
 for i in range(0, groundLvl):
     for j in range(0, w):
-        if(tuple(data[i, j]) == red):
+        if(tupleData[i][j] == red):
             meteors += 1
             if(j in waterCols):
                 fallOnWater += 1
-        if(tuple(data[i, j]) == white):
+        if(tupleData[i][j] == white):
             stars += 1
 
 end = time()
