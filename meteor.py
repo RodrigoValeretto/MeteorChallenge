6477e32008d99d41e19174fa457b7cf5bf50b5c8
rodrigovaleretto <rodrigovaleretto@usp.br>
2021-07-08 11:57:10 -0300

    Código pronto e comentado

--- meteor.py
+++ meteor.py
@@ -6,6 +6,7 @@ img = Image.open('meteor_challenge_01.png')
 data = np.asarray(img)
 
 # Definição das cores de comparação do programa
+# Usamos tuplas para evitar problemas de comparação com vetores
 white = (255, 255, 255, 255)
 black = (0, 0, 0, 255)
 red = (255, 0, 0, 255)
@@ -15,6 +16,7 @@ blue = (0, 0, 255, 255)
 coords = []
 waterCols = []
 
+# Variáveis que salvam o indice do nível do chão e da água
 groundLvl = -1
 waterLevel = -1
 
@@ -26,11 +28,16 @@ fallOnWater = 0
 # Salvando largura e comprimento da matriz
 h, w, _ = data.shape
 
+# Encontra o nível do chão
+# Fizemos aqui uma estimativa pois ao observar a imagem percebemos que próximo ao centro
+# possuímos um nível mais alto de chão podendo cortar mais iterações
+wcentro = int(w/2)
 for i in range(0, h):
-    if(tuple(data[i, 352]) == black):
+    if(tuple(data[i, wcentro]) == black):
         groundLvl = i
         break
 
+# Encontra o nível da água
 for i in range(0, h):
     if(waterLevel != -1):
         break
@@ -39,13 +46,14 @@ for i in range(0, h):
             waterLevel = i
             break
 
-
+# Encontra colunas em que há bits com água
 for i in range(0, w):
     if(tuple(data[waterLevel, i]) == blue):
         waterCols.append(i)
 
 
 # Itera para contar quantidade de estrelas e meteoros
+# Usa o nível do chão para parar de iterar pois a partir do chão não há mais estrelas
 for i in range(0, groundLvl):
     for j in range(0, w):
         if(tuple(data[i, j]) == red):
@@ -55,10 +63,14 @@ for i in range(0, groundLvl):
             stars += 1
 
 # itera para verificar a trajetória dos meteoros e ver se atingem a terra
+# Usa o vetor de colunas de bits de água para verificar se algum dos meteoros possui
+# o indice da coluna igual a um indice desse vetor, se sim significa que ele vai cair
+# ná água.
 for i in coords:
     if(i[1] in waterCols):
         fallOnWater += 1
 
+# Exibe os resultados
 print("O número de meteoros encontrados foi: ", meteors)
 print("O número de estrelas encontradas foi: ", stars)
 print("O número de meteoros que cairão na água é: ", fallOnWater)
